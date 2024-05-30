04a02c1042f1019fdb8381af969e194625389096
Rhoyman Flor <rhoyman12flor@gmail.com>
2024-05-30 13:13:48 -0500

    Parcial Final POO listo fin

---
+++ Documents/NetBeansProjects/Principal/src/principal/Competencia.java
@@ -0,0 +1,43 @@
+package principal;
+
+import java.util.ArrayList;
+
+class Competencia {
+    private ArrayList<Atleta> atletas;
+
+    public Competencia() {
+        this.atletas = new ArrayList<>();
+    }
+
+    public void registrarAtleta(Atleta atleta) {
+        atletas.add(atleta);
+    }
+
+    public Atleta obtenerCampeon() {
+        Atleta campeon = atletas.get(0);
+        for (Atleta atleta : atletas) {
+            if (atleta.getTiempo() < campeon.getTiempo()) {
+                campeon = atleta;
+            }
+        }
+        return campeon;
+    }
+
+    public ArrayList<Atleta> obtenerAtletasPorPais(String nacionalidad) {
+        ArrayList<Atleta> atletasPorPais = new ArrayList<>();
+        for (Atleta atleta : atletas) {
+            if (atleta.getNacionalidad().equalsIgnoreCase(nacionalidad)) {
+                atletasPorPais.add(atleta);
+            }
+        }
+        return atletasPorPais;
+    }
+
+    public double calcularTiempoPromedio() {
+        double sumTiempo = 0;
+        for (Atleta atleta : atletas) {
+            sumTiempo += atleta.getTiempo();
+        }
+        return sumTiempo / atletas.size();
+    }
+}
